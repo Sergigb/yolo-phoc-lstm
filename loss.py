90e78cdd06e0fee6d9f2950a082aa538d74381cf
Sergi Garcia <sergi.garciab@e-campus.uab.cat>
2020-02-28 12:03:30 +0100

    new loss thingie

--- loss.py
+++ loss.py
@@ -1,36 +1,33 @@
-import torch.nn as nn
 import torch
 
 
 class Loss(torch.nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
-        # self.regloss = nn.L1Loss()
-        self.regloss = nn.MSELoss()
-        self.entropy_loss = torch.nn.BCEWithLogitsLoss()
+        self.bceloss = torch.nn.BCEWithLogitsLoss()
+        self.bceloss2 = torch.nn.BCEWithLogitsLoss(reduction="none")
 
-    def forward(self, preds_loc, preds_class, labels, lambda_coord, lambda_noobj):
-        masked_bbox = preds_loc[:, :, 0:4] * labels[:, :, 4].unsqueeze(2)
-        masked_bbox[masked_bbox == 0] = 1.0  # set the masked values to be 1 to avoid problems during the backprop
-        masked_labels = labels[:, :, 0:4] * labels[:, :, 4].unsqueeze(2)
-        masked_labels[masked_labels == 0] = 1.0
+    def forward(self, predicted, gt, lambd=7):
+        # loss = torch.zeros(1).cuda()
+        # really slow, there must be a faster way
+        # for i in range(predicted.shape[0]):
+        #     for j in range(predicted.shape[1]):
+        #         if torch.sum(gt[i, j, :]) > 0:
+        #             loss += self.bceloss(predicted[i, j, :], gt[i, j, :]) * lambd
+        #         else:
+        #             loss += self.bceloss(predicted[i, j, :], gt[i, j, :])
+        # loss /= (predicted.shape[0] * predicted.shape[1])
+        #
+        # print("slow method: ", loss)
 
-        # xy_reg_loss = (masked_bbox[:, :, 0:2] - masked_labels[:, :, 0:2]) ** 2.
-        # xy_reg_loss = torch.mean(xy_reg_loss)
-        # wh_reg_loss = (torch.sqrt(torch.exp(masked_bbox[:, :, 2:4])) - torch.sqrt(torch.exp(masked_labels[:, :, 2:4]))) ** 2.
-        # wh_reg_loss = torch.mean(wh_reg_loss)
+        bcel = self.bceloss(predicted, gt)
+        bcel2 = bcel * lambd
 
-        obj_loss = self.entropy_loss(preds_class[:, :, 0], labels[:, :, 4])
-        # obj_loss = torch.tensor([0]).type(torch.FloatTensor).cuda()
+        loss2 = torch.mean(torch.where(gt > 0, bcel2, bcel))
 
-        xy_reg_loss = self.regloss(masked_bbox[:, :, 0:4], masked_labels[:, :, 0:4])
-        # wh_reg_loss = self.regloss(torch.sqrt(masked_bbox[:, :, 2:4]),
-        #                            torch.sqrt(masked_labels[:, :, 2:4]))
+        # print("fast? method: ", loss2)
 
-        bbox_loss = xy_reg_loss  # + wh_reg_loss
 
-        # bbox_loss = (self.entropy_loss(masked_bbox[0:2], masked_labels[0:2]) +
-        #              self.entropy_loss(masked_bbox[2:4], masked_labels[2:4])) / 2
 
-        return obj_loss + 100*bbox_loss, (obj_loss, 100*bbox_loss, 0)
+        return loss2
 
